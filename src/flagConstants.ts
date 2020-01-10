fa5933ca3571c21c31345b1987f9e64c595fb4db
brisberg <brandon.risberg@gmail.com>
2020-01-10 12:40:06 -0800

    Added a file to hold Flag color encoding constants

---
+++ src/flagConstants.ts
@@ -0,0 +1,6 @@
+/**
+ * Set of constants which control what the colors of flags signify.
+ */
+
+// Reserving the Yellow flag color for energy nodes
+export const ENERGY_NODE_FLAG_COLOR = COLOR_YELLOW;
