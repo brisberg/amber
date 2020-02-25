f51d32f69548df27844c4c173ea5f9151bbe421b
brisberg <brandon.risberg@gmail.com>
2020-02-25 10:57:23 -0800

    Upgraded Rollup multi entry plugin to official repo

--- rollup.test-integration-config.js
+++ rollup.test-integration-config.js
@@ -5,7 +5,7 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from 'rollup-plugin-typescript2';
 import buble from 'rollup-plugin-buble';
-import multiEntry from 'rollup-plugin-multi-entry';
+import multiEntry from '@rollup/plugin-multi-entry';
 import nodent from 'rollup-plugin-nodent';
 
 export default {
