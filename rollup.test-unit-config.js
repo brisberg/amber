13505f7b1aaba284009ed637c62842589803b6c4
brisberg <brandon.risberg@gmail.com>
2020-02-26 10:45:20 -0800

    Replaced rollup-plugin-typescript with official @rollup/plugin-typescript

--- rollup.test-unit-config.js
+++ rollup.test-unit-config.js
@@ -3,7 +3,7 @@
 import clear from 'rollup-plugin-clear';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
-import typescript from 'rollup-plugin-typescript2';
+import typescript from '@rollup/plugin-typescript';
 import buble from 'rollup-plugin-buble';
 import multiEntry from '@rollup/plugin-multi-entry';
 
