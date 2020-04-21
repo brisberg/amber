15bc3e9e64c0267a14a5cfd7dca380ed147a1ef5
brisberg <brandon.risberg@gmail.com>
2020-04-21 09:01:26 -0700

    Added a simple test for tower shooting

---
+++ src/towers/tower.spec.ts
@@ -0,0 +1,30 @@
+import './tower';
+
+import {mockGlobal, mockInstanceOf, mockStructure} from 'screeps-jest';
+
+const towerB = global.tower;
+
+describe('tower module', () => {
+  describe('runTower', () => {
+    it('should attack the nearest hostile creep, if there is one in the room',
+       () => {
+         mockGlobal<Memory>('Memory', {
+           rooms: {
+             'W1N1': {damaged: undefined},
+           },
+         });
+         const hostileCreep = mockInstanceOf<Creep>();
+         const tower = mockStructure(STRUCTURE_TOWER, {
+           attack: () => OK,
+           room: {
+             name: 'W1N1',
+             find: (type: FindConstant): Creep[] | null => {
+               return type === FIND_HOSTILE_CREEPS ? [hostileCreep] : null;
+             },
+           },
+         });
+         towerB.run(tower);
+         expect(tower.attack).toHaveBeenCalledWith(hostileCreep);
+       });
+  });
+});
