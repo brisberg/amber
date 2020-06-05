d5a79facc03286fe67cac72fa899a5e9999cc105
brisberg <brandon.risberg@gmail.com>
2020-06-05 15:11:02 -0700

    Striped SingleMiningMsn to minimum unimplemented shell

--- src/behaviors_v2/missions/mining/single-harvest.ts
+++ src/behaviors_v2/missions/mining/single-harvest.ts
@@ -1,9 +1,8 @@
 import {WORKER} from 'spawn-system/bodyTypes';
+import Mission from '../mission';
 
-export interface SingleHarvestMsnMemory {
-  creep?: string;        // Creep name
-  replacement?: string;  // Creep name
-  sourceIdx: number;
+interface SingleHarvestMsnData {
+  sourceIdx: number;  // Target source index in room.sources
 }
 
 /**
@@ -12,73 +11,24 @@ export interface SingleHarvestMsnMemory {
  *
  * If the position contains a container, the creep will repair it when damaged.
  */
-export default class SingleHarvestMsn {
-  private flag: Flag;
+export default class SingleHarvestMsn extends Mission<SingleHarvestMsnData> {
   private source: Source|null = null;
-  private container: StructureContainer|null = null;
-  private creep: Creep|null = null;
-  private replacement: Creep|null = null;
 
-  protected name: string;
-  protected mem: SingleHarvestMsnMemory;
+  protected bodyType = WORKER;
 
-  constructor(flag: Flag) {
-    this.flag = flag;
-    this.name = flag.name;
-    this.mem = Memory.missions[this.name];
+  protected initMemory(): SingleHarvestMsnData {
+    throw new Error('Method not implemented.');
   }
 
-  public init(): void {
-    const room = this.flag.room;
-
-    if (room) {
-      this.source = room.find(FIND_SOURCES)[this.mem.sourceIdx];
-      this.creep = Game.creeps[this.mem.creep || ''] || null;
-      this.replacement = Game.creeps[this.mem.replacement || ''] || null;
-
-      const conts = this.flag.pos.findInRange(FIND_MY_STRUCTURES, 0, {
-        filter: {
-          structureType: STRUCTURE_CONTAINER,
-        },
-      });
-      if (conts.length > 0) {
-        this.container = conts[0] as unknown as StructureContainer;
-      }
-    }
+  protected get maxCreeps(): number {
+    throw new Error('Method not implemented.');
   }
 
-  public valid(): boolean {
-    if (!this.source) {
-      return false;
-    }
-
-    return true;
+  protected creepActions(): void {
+    throw new Error('Method not implemented.');
   }
 
-  public run(): void {
-    // Purge old names
-    // if (!Game.creeps[this.mem.creep])
-
-    // Request new creeps
-    if (!this.creep) {
-      this.mem.creep = global.spawnQueues[this.flag.pos.roomName].requestCreep({
-        bodyOptions: {max: {work: 6}},
-        bodyRatio: WORKER,
-        mission: this.name,
-        priority: 1,
-      });
-      return;
-    }
-
-    if ((this.creep.ticksToLive || CREEP_LIFE_TIME) < 100 &&
-        !this.mem.replacement) {
-      this.mem.replacement =
-          global.spawnQueues[this.flag.pos.roomName].requestCreep({
-            bodyOptions: {max: {work: 6}},
-            bodyRatio: WORKER,
-            mission: this.name,
-            priority: 1,
-          });
-    }
+  protected finalize(): void {
+    throw new Error('Method not implemented.');
   }
 }
