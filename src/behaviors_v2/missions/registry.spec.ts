2c04d3665eb7d58fe332c872be9648475edaedc4
brisberg <brandon.risberg@gmail.com>
2020-06-07 17:57:52 -0700

    Added TODO tests for Global Registry

---
+++ src/behaviors_v2/missions/registry.spec.ts
@@ -0,0 +1,15 @@
+describe('Mission Registry', () => {
+  it.todo('should initialize itself on the global scope');
+
+  it.todo('should initialize missions from existing flags');
+
+  it.todo('should refresh from flags?');
+
+  it.todo('should register a new mission');
+
+  it.todo('should fetch a registered mission');
+
+  it.todo('should return null when fetching a non-existant mission');
+
+  it.todo('should unregister a mission');
+});
