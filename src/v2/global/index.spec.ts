46618e00c833d434e7cf5f158f7d59ddd354075f
brisberg <brandon.risberg@gmail.com>
2020-06-09 19:56:48 -0700

    Added TODO tests for setupGlobal

---
+++ src/v2/global/index.spec.ts
@@ -0,0 +1,7 @@
+describe('SetupGlobals', () => {
+  it.todo('should initialize the Mission Registry');
+
+  it.todo('should initialize the Behavior Map');
+
+  it.todo('should add Console Commands to global');
+});
