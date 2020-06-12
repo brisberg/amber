91c2884cf9d77fe4b6b6185432b47d460d06366c
brisberg <brandon.risberg@gmail.com>
2020-06-11 17:45:32 -0700

    Added tests for single-harvest mission

--- src/v2/missions/mining/single-harvest.spec.ts
+++ src/v2/missions/mining/single-harvest.spec.ts
@@ -5,22 +5,29 @@ import {mockSpawnQueueInstance} from '../mission.spec';
 
 import SingleHarvestMsn from './single-harvest';
 
-describe.skip('SingleHarvestMsn', () => {
+describe('SingleHarvestMsn', () => {
   beforeEach(() => {
     // Set up Globals
     mockGlobal<{[roomname: string]: SpawnQueue}>('spawnQueues', {
       'N1W1': mockSpawnQueueInstance(),
     });
+    Memory.missions = {};
   });
 
   it('should use max 1 worker', () => {
     // TODO: Should figure out a better way to test this
     const creep = mockInstanceOf<Creep>({name: 'creep1'});
-    const msn = new SingleHarvestMsn('harvest');
+    const msn = new SingleHarvestMsn('harvest').init('N1W1', {});
     msn.assignCreep(creep);
 
     msn.rollCall();
 
     expect(global.spawnQueues['N1W1'].requestCreep).not.toHaveBeenCalled();
   });
+
+  it.todo('should request a replacement when the first worker is old');
+
+  it.todo('should send creeps to harvest the Ith Source in the room');
+
+  it.todo('should send the replacement creep to relieve the worker');
 });
