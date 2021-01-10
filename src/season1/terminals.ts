c1dd9bed5958772260770a05a55652dfdaafbd2a
brisberg <brandon.risberg@gmail.com>
2021-01-10 10:32:34 -0800

    Season1: Moved score aggregation through terminals back to E1S29

--- src/season1/terminals.ts
+++ src/season1/terminals.ts
@@ -1,20 +1,20 @@
 /**
  * This file is a temporary solution to create a one-way transfer of SCORE from
- * Terminal to terminal of 'E6S28'.
+ * Terminal to terminal of 'E1S29'.
  */
 
 import {RESOURCE_SCORE} from './types';
 
 /**
  * operateTerminals takes a room, pushes score from the Terminal to the Terminal
- * in 'E6S28'.
+ * in 'E1S29'.
  */
 export function operateTerminals(room: Room): void {
-  if (!room.terminal || room.name === 'E6S28') {
+  if (!room.terminal || room.name === 'E1S29') {
     return;
   }
 
-  const destT = Game.rooms['E6S28'].terminal;
+  const destT = Game.rooms['E1S29'].terminal;
 
   if (!destT) {
     return;
@@ -23,6 +23,6 @@ export function operateTerminals(room: Room): void {
   // Add a buffer (Limit transfers to 15k)
   const amount = Math.min(room.terminal.store[RESOURCE_SCORE], 15000);
   if (amount >= 10000) {
-    room.terminal.send(RESOURCE_SCORE, amount, 'E6S28');
+    room.terminal.send(RESOURCE_SCORE, amount, 'E1S29');
   }
 }
