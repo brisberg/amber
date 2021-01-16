e9f0a6525aefb15ecfe8412a900d6e052d0a0f13
brisberg <brandon.risberg@gmail.com>
2021-01-15 22:24:26 -0800

    Season1: Moved terminals again to gather in E5S28 (after I've lost E1S29)

--- src/season1/terminals.ts
+++ src/season1/terminals.ts
@@ -1,20 +1,20 @@
 /**
  * This file is a temporary solution to create a one-way transfer of SCORE from
- * Terminal to terminal of 'E1S29'.
+ * Terminal to terminal of 'E5S28'.
  */
 
 import {RESOURCE_SCORE} from './types';
 
 /**
  * operateTerminals takes a room, pushes score from the Terminal to the Terminal
- * in 'E1S29'.
+ * in 'E5S28'.
  */
 export function operateTerminals(room: Room): void {
-  if (!room.terminal || room.name === 'E1S29') {
+  if (!room.terminal || room.name === 'E5S28') {
     return;
   }
 
-  const destT = Game.rooms['E1S29'].terminal;
+  const destT = Game.rooms['E5S28'].terminal;
 
   if (!destT) {
     return;
@@ -23,6 +23,6 @@ export function operateTerminals(room: Room): void {
   // Add a buffer (Limit transfers to 15k)
   const amount = Math.min(room.terminal.store[RESOURCE_SCORE], 15000);
   if (amount >= 10000) {
-    room.terminal.send(RESOURCE_SCORE, amount, 'E1S29');
+    room.terminal.send(RESOURCE_SCORE, amount, 'E5S28');
   }
 }
